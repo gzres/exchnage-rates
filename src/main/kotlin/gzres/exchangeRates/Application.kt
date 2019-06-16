28c35f100a99f9b290c2a325a0d05284303a5ed1
Grzegorz Zulewski <zulewski.grzegorz@gmail.com>
2019-06-16 22:28:20 +0200

    Added starting point

---
+++ src/main/kotlin/gzres/exchangeRates/Application.kt
@@ -0,0 +1,11 @@
+package gzres.exchangeRates
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplicationBuilder(Application::class.java).run(*args)
+}
